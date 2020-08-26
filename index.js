3f5d837cba05435a5ccb158dd4dd7ee28ba13c24
pauladarias <pauladarias@gmail.com>
2020-08-26 11:47:03 +0100

    Refactor displayBooks

--- index.js
+++ index.js
@@ -10,13 +10,21 @@ async function getBooks() {
   });
 }
 
-getBooks().then((books) => {
-  let sampleBook = books[0];
-  console.log(sampleBook);
-
-  document.body.innerHTML = `<div class="my-book">
-      <div class="my-book-cover">${sampleBook.title}</div>
+function getBookHtml(book) {
+  return `<div class="my-book">
+      <div class="my-book-cover">${book.title}</div>
       <div class="my-book-spine"></div>
       <div class="my-book-footer"></div>
   </div>`;
-});
+}
+
+function displayBooks(books) {
+  let sampleBook = books[0];
+  console.log(sampleBook);
+
+  document.body.innerHTML = `<div class="my-library">
+      ${books.map(getBookHtml).join("")}
+  </div>`;
+}
+
+getBooks().then(displayBooks);
